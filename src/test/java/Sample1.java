84dbcd09f8d0fa8e4f740ec29cf2e0163f26a8d2
lakshmirpem <lakshmirpem20@gmail.com>
2023-06-01 15:15:12 +0530

    commit new2

--- src/test/java/Sample1.java
+++ src/test/java/Sample1.java
@@ -4,7 +4,9 @@ public class Sample1
 public static void main(String[] args) 
 {
 	//Welcome to this application
+	
 	System.out.println("hi");
 	System.out.println("bye");
+	System.out.println("Jyothi");
 }
 }
